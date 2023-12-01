828cc231263fc533cae48b2cc593f318cb416096
Nico-242 <Nick9924@icloud.com>
2023-12-01 14:43:13 -0500

    deleted survey and implemented it in evaluations

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,27 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_30_165618) do
     t.integer "user_id", null: false
     t.integer "presentation_id", null: false
     t.integer "survey_id", null: false
+    t.integer "clear_topic_intro", null: false
+    t.integer "clear_direction_intro", null: false
+    t.integer "logical_sequence", null: false
+    t.integer "good_summary_conclusion", null: false
+    t.integer "enough_information", null: false
+    t.integer "free_from_filler", null: false
+    t.integer "filled_time", null: false
+    t.integer "easily_follow", null: false
+    t.integer "presenter_clear_understanding", null: false
+    t.integer "consistent_findings", null: false
+    t.integer "questions_answered", null: false
+    t.integer "effective_visuals", null: false
+    t.integer "legible_visuals", null: false
+    t.integer "proper_grammar", null: false
+    t.integer "visuals_enhance", null: false
+    t.integer "appropriate_timing_visuals", null: false
+    t.integer "clear_delivery", null: false
+    t.integer "effective_body_language", null: false
+    t.integer "eye_contact", null: false
+    t.integer "overall_score", null: false
+    t.text "comments"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["presentation_id"], name: "index_evaluations_on_presentation_id"
