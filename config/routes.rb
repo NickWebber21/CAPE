857df272dbaa4dc60e64283abf4d1e6d0cac91c7
Nick Webber <webber.147@buckeyemail.osu.edu>
2023-12-01 15:19:49 -0500

    added a temporary navbar

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "presentations#index"
+  root "static_pages#home"
+
+  # define other roots
+  get 'help', to: 'static_pages#help', as: 'help'
+  get 'about', to: 'static_pages#about', as: 'about'
+
 end
